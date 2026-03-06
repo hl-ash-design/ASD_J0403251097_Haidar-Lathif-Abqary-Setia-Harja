999e76b3a48e0626afcad5ac42b4561ee8cf3495
katai izano <515haidarlathif.ash@gmail.com>
2026-03-06 15:23:48 +0700

    Menambahkan folder Praktikum6

---
+++ Praktikum6/InsertionSort(A).py
@@ -0,0 +1,13 @@
+def insertionSort(data):
+    for index in range(1,len(data)):
+        currentValue = data[index]
+        position = index
+
+        while position>0 and data[position-1]>currentValue:
+            data[position]=data[position-1]
+            position = position-1
+            data[position] = currentValue
+
+data = [54,26,93,17,77,31,44,55,20]
+insertionSort(data)
+print(data)
